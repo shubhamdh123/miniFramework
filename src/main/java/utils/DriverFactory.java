c68f19ac87889b056a482302ffdcbca2612af2cc
Shubhamdh <shubhd1976shubhdh1976@gmail.com>
2025-09-06 12:25:31 +0530

    pom update

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,6 +1,7 @@
 package utils;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
