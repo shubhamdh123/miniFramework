f8afe0d11029a6a518f5fdc987cc61d22e170980
Shubhamdh <shubhd1976shubhdh1976@gmail.com>
2025-09-06 12:09:34 +0530

    pom update

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,23 +1,28 @@
 package utils;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
-	private static Properties properties =new Properties();
-	
-	static {
-		try {
-			FileInputStream fis =new FileInputStream("src/resources/config.properties");
-			properties.load(fis);
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	public static String get(String key) {
-		return properties.getProperty(key);
-	}
+    private static Properties properties = new Properties();
 
+    static {
+        try (InputStream input = ConfigReader.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in classpath!");
+            }
+            
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return properties.getProperty(key);
+    }
 }
