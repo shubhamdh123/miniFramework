6116354b3a6bcb0965db9b060b0f1671562bc7ef
Shubhamdh <shubhd1976shubhdh1976@gmail.com>
2025-09-06 12:15:45 +0530

    pom update

--- src/main/java/utils/ExcelUtil.java
+++ src/main/java/utils/ExcelUtil.java
@@ -1,6 +1,6 @@
 package utils;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Row;
@@ -9,36 +9,33 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelUtil {
-	public static Object[][] getTestData(String filePath,String sheetName)
-	{
-		
-	Object[][] data= null;
-	try {
-		FileInputStream fis = new FileInputStream(filePath);
-		Workbook workbook =WorkbookFactory.create(fis);
-		Sheet sheet =workbook.getSheet(sheetName);
-		
-		int rows = sheet.getPhysicalNumberOfRows();
-		int cols = sheet.getRow(0).getLastCellNum();
-		
-		data =new Object[rows-1][cols]; //skip header row
-		
-		for (int i=1;i<rows;i++) {
-			Row row =sheet.getRow(i);
-			
-			for(int j=0;j<cols;j++) 
-			{
-				data[i-1][j]= row.getCell(j).toString();
-			}
-		}
-		
-		workbook.close();
-				}
-	catch(IOException e) 
-	{
-		e.printStackTrace();
-	}
-	return data;
-}
 
+    public static Object[][] getTestData(String fileName, String sheetName) {
+        Object[][] data = null;
+        try (InputStream is = ExcelUtil.class.getClassLoader().getResourceAsStream(fileName)) {
+            if (is == null) {
+                throw new RuntimeException("File not found in classpath: " + fileName);
+            }
+
+            Workbook workbook = WorkbookFactory.create(is);
+            Sheet sheet = workbook.getSheet(sheetName);
+
+            int rows = sheet.getPhysicalNumberOfRows();
+            int cols = sheet.getRow(0).getLastCellNum();
+
+            data = new Object[rows - 1][cols]; // skip header row
+
+            for (int i = 1; i < rows; i++) {
+                Row row = sheet.getRow(i);
+                for (int j = 0; j < cols; j++) {
+                    data[i - 1][j] = row.getCell(j).toString();
+                }
+            }
+
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
 }
